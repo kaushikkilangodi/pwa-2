4e02e3cd743dbf407366e1af2c1b2edadea8d140
kaushikkilangodi <kaushikk883@gmail.com>
2024-05-28 09:19:35 +0530

    update

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <VitePwaManifest/>
+    <NuxtPwaManifest />
     <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,81 +1,66 @@
+import process from 'node:process'
+
+const sw = process.env.SW === 'true'
 
 export default defineNuxtConfig({
-  
-  modules: ["@vite-pwa/nuxt"],
-  pwa:{
-    manifest:{
-      name:"nuxt 3PWA",
-      short_name:"nuxt 3 pwa",
-      theme_color :"#000000",
-      
-  
-      icons:[
-      {
-        src: "icons/icon_64x64.png",
-        sizes: "64x64",
-        type: "image/png",
-        purpose:"maskable"
-      },
-      {
-        src: "icons/icon_144x144.png",
-        sizes: "144x144",
-        type: "image/png",
-      },
-      {
-        src: "icons/icon_192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "icons/icon_512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },],
-      description:"testing",
-      screenshots:[{
-        src:"icons/home.png",
-        sizes:"512x512",
-        type:"image/png",
-        form_factor:"wide",
 
+  /* ssr: false, */
+  // typescript,
+  modules: ['@vite-pwa/nuxt'],
+  nitro: {
+    esbuild: {
+      options: {
+        target: 'esnext',
       },
-      {
-        src:"icons/install.png",
-        sizes:"144x144",
-        type:"image/png",
-        form_factor:"narrow",
-        
-      },]
     },
-    workbox: {
-      runtimeCaching: [
+  },
+ 
+  pwa: {
+    strategies: sw ? 'injectManifest' : 'generateSW',
+    srcDir: sw ? 'service-worker' : undefined,
+    filename: sw ? 'sw.ts' : undefined,
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Nuxt Vite PWA',
+      short_name: 'NuxtVitePWA',
+      theme_color: '#ffffff',
+      icons: [
         {
-          urlPattern: ({ request }) => request.destination === 'document' || request.destination === 'script' || request.destination === 'style',
-          handler: 'StaleWhileRevalidate',
-          options: {
-            cacheName: 'static-resources',
-          }
+          src: 'icons/icon_192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
         },
         {
-          urlPattern: ({ request }) => request.destination === 'image' || request.destination === 'video',
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'media-resources',
-            expiration: {
-              maxEntries: 50,
-              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-            },
-          },
+          src: 'icons/icon_512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'icons/icon_512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
         },
       ],
     },
-  
-    
-    devOptions:{
-      enabled:true,
-      type:'module',
+    workbox: {
+      globPatterns: ['*/.{js,css,html,png,svg,ico}'],
     },
+    injectManifest: {
+      globPatterns: ['*/.{js,css,html,png,svg,ico}'],
     },
-    ssr:false,
-
-});
+    client: {
+      installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  },
+})
